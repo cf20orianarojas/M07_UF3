6c9679adc532034dcd7533a595c605b0f68030cd
cf20orianarojas <rojasoriana260217@gmail.com>
2024-02-02 15:54:55 +0100

    Exercici 18 millorat

--- exercici18.py
+++ exercici18.py
@@ -3,15 +3,8 @@ Demanar a l’usuari posar 2 paraules. Afegir aquestes a una tupla i
 mostrar per pantalla quantes vegades apareix cada lletra.
 '''
 paraula = input('Posa 2 paraules: ')
-tupla = tuple(paraula.replace(' ',''))
-count = {} # diccionari per comptar
+tupla = tuple(paraula.replace(" ", ""))
+lletres = set(tupla)
 
-for lletra in paraula:
-    if lletra in count:
-        count[lletra]+=1
-    else:
-        count[lletra]=1
-
-for lletra, count in count.items():
-    print(f'{lletra}: {count}')
-print(tupla)
+for l in lletres:
+    print(f'{l}: {tupla.count(l)}')
