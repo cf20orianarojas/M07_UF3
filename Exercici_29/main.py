1db48c8be78094c226a7d5d6ac341361a4cd30a3
cf20orianarojas <rojasoriana260217@gmail.com>
2024-02-04 17:22:34 +0100

    Exercici 29 funcio interval i main

--- Exercici_29/main.py
+++ Exercici_29/main.py
@@ -1,10 +1,8 @@
 '''Crear una funció que passats dos números per paràmetre (demanats a l’usuari) ens mostra per pantalla tots 
 els números (enters) que hi ha entre ells. També mostrarà per pantalla la suma d’aquests números enters.'''
-def intervalNum(n1, n2):
-    nums = 'Números: '
-    suma = 0
+from exercici29 import intervalNum
+n1 = int(input('Introdueix un número: '))
+n2 = int(input('Introdueix altre número: '))
 
-    for i in range(n1, n2+1):
-        nums+=f'{i} '
-        suma+=i
-    return f'{nums}\nSuma total: {suma}'
+result = intervalNum(n1, n2)
+print(result)
