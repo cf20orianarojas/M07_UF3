c432972d634a2147b9c46a5ccda5b4e3d1a1432a
cf20orianarojas <rojasoriana260217@gmail.com>
2024-02-04 18:05:10 +0100

    Exerici 31 funcion aplicarIVA i main

--- Exercici_31/main.py
+++ Exercici_31/main.py
@@ -1,10 +1,10 @@
 '''Crear una funció per calcular el total d’una factura amb l’IVA. La funció ha de rebre (per paràmetre) la 
 quantitat sense IVA i l’IVA a aplicar (introduïts per l’usuari). En cas de no passar-li cap valor o un valor 
 erroni (4%, 10% o 21%) s’aplicarà directament el 21%. Es mostra per pantalla el valor sense IVA, l’IVA i el total.'''
-def aplicarIVA(valor, iva):
-    if iva not in [4, 10, 21]:
-        print('L’IVA a aplicar no és vàlid només 4%, 10% i 21%, s’aplicarà el 21%\n')
-        iva = 21
-    tasa = iva / 100
-    total = valor + (valor*tasa)
-    return total
+from exercici31 import aplicarIVA
+
+valor = int(input('Introduce un valor en euros: '))
+iva = int(input('Introduce el IVA a aplicar (4%, 10%, o 21%): '))
+
+total = aplicarIVA(valor, iva)
+print(f'Total amb l’IVA: {total}€')
