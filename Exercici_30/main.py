9683739782b9791bd198b13559c5aef092466576
cf20orianarojas <rojasoriana260217@gmail.com>
2024-02-04 17:28:56 +0100

    Exercici 30 main

---
+++ Exercici_30/main.py
@@ -0,0 +1,5 @@
+# Crear una funció que passat un nom per l’usuari (nom). Mostri per pantalla “Hola nom”.
+from exercici30 import holaNom
+nom = input('Com et dius?: ')
+result = holaNom(nom)
+print(result)
