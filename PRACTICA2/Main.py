afaf782cc6b887c4dd3dc73784aa81e2a3d282c5
cf20orianarojas <rojasoriana260217@gmail.com>
2024-02-09 17:27:46 +0100

    Main.py casi acabado

--- PRACTICA2/Main.py
+++ PRACTICA2/Main.py
@@ -6,27 +6,34 @@ from read import read_query
 from update import update_query
 from delete import delete_query
 
-
 try:
-    operacion = input("¿Qué operación CRUD quieres realizar? (T: Crear tabla, C: Crear Registro R: Leer, U: Actualizar, D: Eliminar): ").upper()
+    operacion = input("¿Qué operación de CRUD quieres realizar? (T: Crear tabla, C: Crear Registro R: Leer, U: Actualizar, D: Eliminar): ").upper()
 
-    # operacions de CRUD
-    options = ['T','C','R','U','D']
+    # operacions de CRUD 
+    operaciones = {
+        'T': crea_taula(),
+        'C': create_query(),
+        'R': read_query(),
+        'U': update_query(),
+        'D': delete_query()
+    }
 
+    if operacion not in operaciones:
+        print('Operación no válida')
+    else:
+        # La query sera la correspondiente a la operación seleccionada
+        query = operaciones[operacion]
 
-    # table = crea_taula()
-    # create = create_query()
-    # read = read_query()
-    # update = update_query()
-    # delete = delete_query()
+        print(query)
     
     # execute envia la query corresponent (table, create, read, update, delete)
-    
     connection.execute(query)
+    print(connection)
 
     # fa efectius els canvis de la query.
     conn.commit()
 except (Exception, psycopg2.Error) as error:
     print('Error', error)
 finally:
+    connection.close()
     conn.close()
